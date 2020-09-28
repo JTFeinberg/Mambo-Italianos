5d761ef8852224f6ed8b605aa20fd4b945fd07f7
Jacob Feinberg <JacobFeinberg92@gmail.com>
2020-09-27 23:25:15 -0400

    Create a Layout component

---
+++ gatsby/src/components/Layout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Nav from './Nav';
+
+export default function Layout() {
+  return (
+    <div>
+      <Nav />
+      <p>Some page content here</p>
+    </div>
+  );
+}
