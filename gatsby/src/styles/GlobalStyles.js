b46e15c354d1dba1234b985709f180555bba3d0c
Jacob Feinberg <JacobFeinberg92@gmail.com>
2020-09-28 10:01:25 -0400

    Add styles to html,body,button,fieldset,images

---
+++ gatsby/src/styles/GlobalStyles.js
@@ -0,0 +1,52 @@
+import { createGlobalStyle } from 'styled-components';
+import bg from '../assets/images/bg.svg';
+import stripes from '../assets/images/stripes.svg';
+
+const GlobalStyles = createGlobalStyle`
+  :root {
+    --red: #FF4949;
+    --black: #2E2E2E;
+    --yellow: #ffc600;
+    --white: #fff;
+    --grey: #efefef;
+  }
+  html {
+    background-image: url(${bg});
+    background-size: 450px;
+    background-attachment: fixed;
+    font-size: 10px;
+  }
+
+  body {
+    font-size: 2rem;
+  }
+
+  fieldset {
+    border-color: rgba(0,0,0,0.1);
+    border-width: 1px;
+  }
+
+  button {
+    background: var(--red);
+    color: white;
+    border: 0;
+    padding: 0.6rem 1rem;
+    border-radius: 2px;
+    cursor: pointer;
+    --cast: 2px;
+    box-shadow: var(--cast) var(--cast) 0 var(--grey);
+    text-shadow: 0.5px 0.5px 0 rgba(0,0,0,0.2);
+    transition: all 0.2s;
+    &:hover {
+      --cast: 4px;
+    }
+  }
+
+  .gatsby-image-wrapper img[src*=base64\\,] {
+    image-rendering: -moz-crisp-edges;
+    image-rendering: pixelated;
+  }
+
+
+
+export default GlobalStyles;
