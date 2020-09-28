05703f7d7576044b9e47c52f93510a5a27deabe6
Jacob Feinberg <JacobFeinberg92@gmail.com>
2020-09-27 23:31:11 -0400

    Add Footer to Layout

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Nav from './Nav';
+import Footer from './Footer';
 
 export default function Layout() {
   return (
     <div>
       <Nav />
       <p>Some page content here</p>
+      <Footer />
     </div>
   );
 }
