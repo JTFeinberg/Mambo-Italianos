37a452b44135bf75a98ece05a87c5c50ff4ad7d9
Jacob Feinberg <JacobFeinberg92@gmail.com>
2020-09-28 10:09:48 -0400

    Create Typography scaffolding

---
+++ gatsby/src/styles/Typography.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+
+import font from '../assets/fonts/frenchfries.woff';
+
+const Typography = createGlobalStyle`
+
+`;
+
+export default Typography;
