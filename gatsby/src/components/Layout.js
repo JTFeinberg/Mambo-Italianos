71bbe1aa73da47494ce227e13567a3ea78247ee9
Jacob Feinberg <JacobFeinberg92@gmail.com>
2020-11-22 11:17:26 -0500

    Add media query max width setting

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -15,6 +15,10 @@ const SiteBorderStyles = styled.div`
   padding: 5px;
   padding: clamp(5px, 1vw, 25px);
   box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.044);
+  @media (max-width: 1100px) {
+    margin-left: 1.5rem;
+    margin-right: 1.5rem;
+  }
 `;
 
 const ContentStyles = styled.div`
