1ba6a00d74cd81d6c036c797e44282c79e32caa0
Jacob Feinberg <JacobFeinberg92@gmail.com>
2020-11-22 11:14:56 -0500

    Add and Scalle padding, add stipes background

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -5,11 +5,16 @@ import Nav from './Nav';
 import Footer from './Footer';
 import GlobalStyles from '../styles/GlobalStyles';
 import Typography from '../styles/Typography';
+import stripes from '../assets/images/stripes.svg';
 
 const SiteBorderStyles = styled.div`
   max-width: 1000px;
   margin: 12rem auto 4rem auto;
   margin-top: clamp(2rem, 10vw, 12rem);
+  background: white url(${stripes});
+  padding: 5px;
+  padding: clamp(5px, 1vw, 25px);
+  box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.044);
 `;
 
 const ContentStyles = styled.div`
