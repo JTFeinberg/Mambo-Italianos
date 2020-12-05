1a0a65713124ef9ccbf416706c6d88700087d805
Jacob Feinberg <JacobFeinberg92@gmail.com>
2020-12-05 16:46:38 -0500

    Add icon and slug field to pizza

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -1,15 +1,27 @@
+import { MdLocalPizza as icon } from 'react-icons/md';
+
 export default {
   // Computer Name
   name: 'pizza',
   // Visible Title
   title: 'Pizzas',
   type: 'document',
+  icon,
   fields: [
     {
       name: 'name',
       title: 'Pizza Name',
       type: 'string',
       description: 'Name of the pizza',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
     },
   ],
 };
